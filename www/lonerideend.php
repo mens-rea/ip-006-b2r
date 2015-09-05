c6cf03d97ff53b02580496b6ecee8c9852395287
Juan Miguel G. Alvarez <MY_NAME@example.com>
2015-09-05 14:49:35 -0700

    trying it out

--- www/lonerideend.php
+++ www/lonerideend.php
@@ -22,26 +22,25 @@
           <img class="logo" src="img/b2r.png" />
         </header>
 
-        <div id="map"></div>
-
-        <div class="group-cont lone-cont">
+        <div class="group-cont lone-cont grey-cont">
             <div class="user-dets-cont">
                 <div class="alpha-rider rider-frame">
                     <img src="img/user1.png"/>
                 </div>
+                <h3 class="user-stat">ARRIVED</h3>
                 <h3 class="bike-no"><b>BIKE NO. 004</b></h3>
             </div>
         
           <h3><img class="icons" src="img/group.png"/><b>ID: 0945721</b></h3>
           <h4><img class="icons" src="img/station.png"/>Bench Tower Rizal Drive, Taguig City</h4>
-          <h4><img class="icons icons-small" src="img/time.png"/>Estimated Time: 10 mins</h4>
-          <h4><img class="icons icons-small" src="img/price.png"/>20.00 - 25.00</h4>
+          <h4><img class="icons icons-small" src="img/time.png"/>Time: 20 mins</h4>
+          <h1>Php 22.50</h1>
         </div>     
 
-        <div class="main-form-submit start-button"><a href="lonerideend.php"><i class="fa fa-home"></i>START RIDE!</a></div>
+        <div class="main-form-submit start-button"><a href="lonerideend.php"><i class="fa fa-home"></i>END TRANSACTION</a></div>
      
         
-        <script>
+<script>
             
             function initAutocomplete() {
               
